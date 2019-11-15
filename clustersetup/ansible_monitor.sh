0cf5ab642551672340990b8263b99fa23d456d21
root <root@localhost.localdomain>
2019-11-15 12:27:33 -0500

    Fixed Task Name to search in Error state

--- clustersetup/ansible_monitor.sh
+++ clustersetup/ansible_monitor.sh
@@ -4,12 +4,12 @@
 #count total number of tasks in playbook
 TotalTaskNumber=$(cat ../ansible_playbook/tasks/main.yml | grep "\- name:" |grep -v "#"|wc -l)
 #check last task running
-LastTask=$(cat ../clustersetup/nohup.out | grep  "TASK" | tail -1|tr "\*" " ")
+LastTask=$(cat nohup.out | grep  "TASK" | tail -1|tr "\*" " ")
 #total tasks comleted so far
-var=$(cat ../clustersetup/nohup.out | grep  "TASK" | wc -l)
+var=$(cat nohup.out | grep  "TASK" | wc -l)
 CompletedTasks=$(($var-4))
 #find if there were failed tasks
-isFinished=$(cat ../clustersetup/nohup.out | grep "PLAY RECAP")
+isFinished=$(cat nohup.out | grep "PLAY RECAP")
 
 if [ -z "$isFinished" ];
 then
@@ -19,15 +19,18 @@ else
  echo "Ansible playbook has finished. Let's Check the status now !"
 fi
 
-isfailedTasks=$(cat ../clustersetup/nohup.out | tail -3 | grep -i failed  | awk '{print $6}')
+isfailedTasks=$(cat nohup.out | tail -3 | grep -i failed  | awk '{print $6}')
 patern="failed=0"
 if [ "$isfailedTasks" = "$patern" ];
 then
  echo "Ansible Completed Successfuly"
 else
  #TNM=$(echo "$LastTask"|tr "\[" " "|tr "\]" " ")
- TNM=$(echo "$LastTask"|tr "\[" "'[ ")
- Log=$(cat ../clustersetup/nohup.out | grep  "TNM")
+ #TNM=$(echo "$LastTask"|tr "\[" "'[ " | tr "\]" "'] ")
+ TNM=$(echo "$LastTask"|tr -d "[" | tr -d "]")
+ TaskSearch=$(echo "$TNM" | awk '{$1="" ; print $0}' | awk '{$1=$1};1')
+ echo "############" $TaskSearch
+ Log=$(cat nohup.out | grep  "$TaskSearch" -A 1)
  echo "Error Found in "$TNM" "
  echo "Relevant Logs:"
  echo "$Log"
